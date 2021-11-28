2de7bd2aef85988885ea99d6c98f27f8d1ad0c5c
taipicuri009 <priscilataipicuri009@gmail.com>
2021-11-28 13:10:35 -0500

    subiendo

---
+++ sesion25.js
@@ -0,0 +1,24 @@
+/*let indice = 10;
+while (indice <=100){
+    console.log(indice);
+    indice = indice + 10;
+}
+
+const ositos = ['oso panda','oso de anteojos','oso polar','oso perezoso'];
+let i = 0;
+while (ositos.length > i){
+    console.log(ositos[i]);
+    i = i+1;
+}
+
+let i = 0;
+do{
+    i = i + 1;
+    console.log(1);
+}while(i <= 5)*/
+
+/*const patron = /priscila/;
+let dato;
+do{
+    dato = prompt('ingrese el valor:')
+} while(!patron.test(dato));*/
